fecb3483729d3084c7d407893681f74b8a06e136
Riad Shash <riadshash@yahoo.com>
2018-11-07 12:29:40 -0700

    Added my first clojure program - fib, fibonacci number generator that includes performance results

---
+++ RAY/Clojure/Recursive Fibonacci/fib.clj
@@ -0,0 +1,29 @@
+(println "Welcome the Fibonacci program!") (println)
+
+;Macro definition - Like defn, but the resulting function name is declared as a
+;macro and will be used as a macro by the compiler when it is called.
+(defmacro sectime [expr]
+  `(let [start# (. System (currentTimeMillis))
+         ret# ~expr]
+     ;;;str connects two strings
+     (println (str "Elapsed time: " (/ (double (- (. System (currentTimeMillis)) start#)) 1000.0) " seconds"))
+     ret#))
+
+;;;Counts the number of times fib was called
+(def iter 0)
+
+;Function definition of fib
+(defn fib [n]
+  (def iter (+ iter 1))
+  (if (< n 2)
+   ;;;Base case
+    n
+   ;;;Inductive case
+    (+ (fib (- n 1))
+       (fib (- n 2)))))
+  
+(print "The 30th fibonacci number is: ") (print (fib 30)) (println)
+
+(print "Fib function was called: ") (print iter) (println " times.")
+
+(sectime(fib 30))
